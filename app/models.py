b45655f909c0b00a01764eeb54e51d9a95918f23
picardcharlie <picardcharlie@gmail.com>
2022-06-15 22:58:44 -0500

    confirmation token added

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 #from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import jwt
+from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 
@@ -26,15 +28,16 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def generate_confirmation_token(self, expiration_seconds=3600):
-        s = Serializer(current_app.secret_key, expiration_sec)
-        return s.dumps({"confirm_id": selfid}).decode("uft-8")
+    def generate_confirmation_token(self):
+        expiration_datetime = datetime.utcnow() + timedelta(seconds=3600)
+        data = {"exp": expiration_datetime, "confirm_id": self.id}
+        token = jwt.encode(data, current_app.secret_key, algorithm="HS512")
+        return token
 
     def confirm(self, token):
-        s = Serializer(current_app.secret_key)
         # is token valid and hasn't expired
         try:
-            data = s.loads(token.encode("uft-8"))
+            data = jwt.decode(token, current_app.secret_key, algorithms=["HS512"]) #s.loads(token.encode("uft-8"))
         except:
             return False
         # does the token's data match the users
@@ -42,8 +45,12 @@ class User(UserMixin, db.Model):
             return False
         self.confirmed = True
         db.session.add(self)
+
         # the data isn't commited yet as you want to make sure the user is currently logged in.
         return True
+
+        # ...
+        data = jwt.decode(token, current_app.secret_key, algorithms=["HS512"])
 
     def __init__(self, username, user_email, password, confirmed):
         self.username = username
