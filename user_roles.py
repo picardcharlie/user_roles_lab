cc08d5aeb66fe298d556adc0eea36a92fc97a4aa
picardcharlie <picardcharlie@gmail.com>
2022-11-29 20:13:43 -0600

    fixed migrate

--- user_roles.py
+++ user_roles.py
@@ -5,7 +5,7 @@ from app.models import db, User, Permission, Role
 #db, user, post, etc
 
 app = create_app(os.getenv("FLASK_CONFIG") or 'default')
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 @app.shell_context_processor
 def make_shell_context():
